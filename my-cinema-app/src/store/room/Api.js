3ef82f5d0fdf9691c528d7e47978787a503245c3
Arzoyan <ruben.arzoyan@gmail.com>
2024-06-21 01:33:19 +0400

    add dot env file config

--- my-cinema-app/src/store/room/Api.js
+++ my-cinema-app/src/store/room/Api.js
@@ -2,7 +2,11 @@ import API_URL from "../../config";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const API_ROOMS = `${API_URL || "http://localhost:4000/api"}/rooms/`;
-
+console.log(
+  "%cmy-cinema-appsrcstore\roomApi.js:5 API_URL",
+  "color: #26bfa5;",
+  API_URL,
+);
 export const fetchRooms = createAsyncThunk("rooms/fetchRooms", async () => {
   const response = await fetch(API_ROOMS);
   const data = await response.json();
