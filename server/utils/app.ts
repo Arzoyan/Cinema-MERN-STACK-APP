204f407df72f1d70b773eb6ef646f31afd2ffab1
Arzoyan <ruben.arzoyan@gmail.com>
2024-06-20 20:30:36 +0400

    revert code

--- server/utils/app.ts
+++ server/utils/app.ts
@@ -1,32 +1,9 @@
 import express, { NextFunction, Request, Response } from "express"; // Backend App (server)
 import cors from "cors"; // HTTP headers (enable requests)
 import { ORIGIN } from "../constants";
-import path from "path";
 
 // initialize app
 const app = express();
-
-// Serve static files from the user frontend
-app.use(
-  "/user",
-  express.static(path.join(__dirname, "../../my-cinema-app/build")),
-);
-
-// Serve static files from the admin frontend
-app.use(
-  "/admin",
-  express.static(path.join(__dirname, "../../admin-panel/build")),
-);
-
-// Catchall handler for user frontend
-app.get("/user/*", (_, res) => {
-  res.sendFile(path.join(__dirname, "../../my-cinema-app/build", "index.html"));
-});
-
-// Catchall handler for admin frontend
-app.get("/admin/*", (_, res) => {
-  res.sendFile(path.join(__dirname, "../../admin-panel/build", "index.html"));
-});
 
 // middlewares
 app.use(cors({ origin: ORIGIN }));
